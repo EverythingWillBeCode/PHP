70f462b0773cbef2bdb6e2cbdaaa4bf10d175877
Christian! <67934837+EverythingWillBeCode@users.noreply.github.com>
2024-09-19 20:08:21 -0300

    update

--- Gestion_Prestamos/login.php
+++ Gestion_Prestamos/login.php
@@ -29,6 +29,10 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 <html lang="es">
 <head>
     <meta charset="UTF-8">
+    
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/styles.css">
+    
     <title>Iniciar Sesión</title>
 </head>
 <body>
