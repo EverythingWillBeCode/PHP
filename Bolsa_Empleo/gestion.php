2bc21521c9b06517849a0c9ee55e55067d9dc136
Christian! <67934837+EverythingWillBeCode@users.noreply.github.com>
2024-10-29 14:45:18 -0300

    update

--- Bolsa_Empleo/gestion.php
+++ Bolsa_Empleo/gestion.php
@@ -11,16 +11,43 @@ if (!isset($_SESSION['usuario'])) {
 <head>
     <meta charset="UTF-8">
     <title>Gestión de CRUDs</title>
+    <link rel="stylesheet" href="css/estilos.css"> <!-- Ruta a estilos.css -->
 </head>
 <body>
-    <h2>Bienvenido, <?php echo $_SESSION['usuario']; ?></h2>
-    <p>Seleccione una de las opciones para gestionar:</p>
-    <ul>
-        <li><a href="crud_estudiantes.php">Gestión de Estudiantes</a></li>
-        <li><a href="crud_cursos.php">Gestión de Cursos</a></li>
-        <li><a href="crud_instructores.php">Gestión de Instructores</a></li>
-        <li><a href="crud_empresas.php">Gestión de Empresas</a></li>
-    </ul>
-    <p><a href="logout.php">Cerrar Sesión</a></p>
+    <div class="container">
+        <h2>Bienvenido, <?php echo $_SESSION['usuario']; ?></h2>
+        <p>Seleccione una de las opciones para gestionar:</p>
+
+        <!-- Menú de gestión en Flexbox -->
+        <ul class="menu">
+            <li class="menu-item">
+                <a href="crud_estudiantes.php">
+                    <img src="images/estudiantes.png" alt="Gestión de Estudiantes">
+                    <p>Estudiantes</p>
+                </a>
+            </li>
+            <li class="menu-item">
+                <a href="crud_cursos.php">
+                    <img src="images/cursos.png" alt="Gestión de Cursos">
+                    <p>Cursos</p>
+                </a>
+            </li>
+            <li class="menu-item">
+                <a href="crud_instructores.php">
+                    <img src="images/instructores.png" alt="Gestión de Instructores">
+                    <p>Instructores</p>
+                </a>
+            </li>
+            <li class="menu-item">
+                <a href="crud_empresas.php">
+                    <img src="images/empresas.png" alt="Gestión de Empresas">
+                    <p>Empresas</p>
+                </a>
+            </li>
+        </ul>
+
+        <p><a href="logout.php" class="logout">Cerrar Sesión</a></p>
+    </div>
 </body>
 </html>
+
